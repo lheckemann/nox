82b868bc79a0b615209931213c689d3814e73e61
Linus Heckemann <git@sphalerite.org>
2017-05-02 15:18:22 +0100

    Terrible hack to get version right in expression

--- default.nix
+++ default.nix
@@ -1,6 +1,17 @@
 with import <nixpkgs> { };
 
+let
+  # Beautiful hacks. Don't look.
+  version =
+    let
+      versionMarker = "version = ";
+      setupCfg = builtins.readFile ./setup.cfg;
+      lines = lib.splitString "\n" setupCfg;
+      versionLine = head (filter (lib.hasPrefix versionMarker) lines);
+    in lib.removePrefix versionMarker versionLine;
+in
 stdenv.lib.overrideDerivation nox (oldAttrs : {
   src = ./.;
+  inherit (version);
   buildInputs = oldAttrs.buildInputs ++ [ git ];
 })
